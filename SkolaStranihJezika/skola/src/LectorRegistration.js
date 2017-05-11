308917c15804a7e6febed5ee04e55317985c6fe5
Omar Sokolovic <osokolovic1@etf.unsa.ba>
2017-05-11 15:52:18 +0200

    Registracija predavača

---
+++ SkolaStranihJezika/skola/src/LectorRegistration.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class LectorRegistration extends Component {
+
+    loginValidation(){
+      //ovdje validirati unos što će spriječiti submit ako je neko od polja neispravno
+      return true;
+    }
+
+  render() {
+    return (
+      <div>
+        <form name="LectorRegistrationForm" onSubmit={() => {return this.loginValidation}}>
+          <label>Ime:&nbsp;&nbsp;</label>
+          <input type="text" ref="predavacIme"/>
+
+          <label>Prezime:&nbsp;&nbsp;</label>
+          <input type="text" ref="predavacPrezime"/>
+
+          <label>Broj Telefona:&nbsp;&nbsp;</label>
+          <input type="text" ref="brojTelefona"/>
+
+          <label>E-mail:&nbsp;&nbsp;</label>
+          <input type="text" placeholder="example@example.com" ref="email"/>
+
+          <label>Korisničko Ime:&nbsp;&nbsp;</label>
+          <input type="text" ref="korisnickoIme"/>
+
+          <label>Šifra:&nbsp;&nbsp;</label>
+          <input type="text" ref="sifra"/>
+
+          <label>Ponovite šifru:&nbsp;&nbsp;</label>
+          <input type="text" ref="ponoviSifru"/>
+
+          <label>Jezik i stepen:&nbsp;&nbsp;</label>
+          <select ref="jezikSelect">
+            <option value="njemackiA1">Njemački A1</option>
+            <option value="njemackiA2">Njemački A2</option>
+            <option value="njemackiB1">Njemački B1</option>
+            <option value="njemackiB2">Njemački B2</option>
+            <option value="njemackiC1">Njemački C1</option>
+            <option value="njemackiC2">Njemački C2</option>
+            <option value="engleskiA1">Engleski A1</option>
+            <option value="engleskiA2">Engleski A2</option>
+            <option value="engleskiB1">Engleski B1</option>
+            <option value="engleskiB2">Engleski B2</option>
+            <option value="engleskiC1">Engleski C1</option>
+            <option value="engleskiC2">Engleski C2</option>
+            <option value="spanskiA1">Španski A1</option>
+            <option value="spanskiA2">Španski A2</option>
+            <option value="spanskiB1">Španski B1</option>
+            <option value="spanskiB2">Španski B2</option>
+            <option value="spanskiC1">Španski C1</option>
+            <option value="spanskiC2">Španski C2</option>
+          </select>
+
+          <button type="submit">Registruj</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LectorRegistration;
